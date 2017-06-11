f9f42f4946aa12e24462b05605048faac6e52ef8
Saneef Ansari <hello@saneef.com>
2017-06-12 05:07:56 +0530

    recreates the error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,18 @@ class BooksController < ApplicationController
 	def index
 		@books = Book.all
 	end
+
+	def update
+		@book = Book.find(params[:id])
+
+		@book.update(favorite: !@book.favorite)
+
+		respond_to do |format|
+      format.js
+    end
+	end
+
+	def allowed_params
+    params.require(:feed).permit(:id)
+  end
 end
