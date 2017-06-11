6110d38de36714a4f4188adbdd9026969a6dc829
Saneef Ansari <hello@saneef.com>
2017-06-12 04:22:51 +0530

    adds books model and fixture for it

---
+++ db/migrate/20170611224215_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.boolean :favorite
+
+      t.timestamps
+    end
+  end
+end
